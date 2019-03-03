d6dce54d967799c0c88ee5776708b957aca90524
Gode King <GengGode@outlook.com>
2019-03-03 17:06:41 +0800

    完成ER.m 基于B2S23规则

--- SixGrid_V2.0/EvolutionRules.m
+++ SixGrid_V2.0/EvolutionRules.m
@@ -1,11 +1,31 @@
-function State = EvolutionRules(B,S,AdjacentElements)
+function State = EvolutionRules(AdjacentElements,B,S)
 %EVOLUTIONRULES 判断下一周期元素的状态
-%   此处显示详细说明
+%   State = EvolutionRules(B,S,AdjacentElements)
+%   Input：
+%   AdjacentElements 当前元素的邻居状态
+%   B 由死而生时邻居数
+%   S 由生而死时邻居数
+%   Output：
+%   State 返回值为下一时刻元素状态
+
+%   GengGode_2019.03.02
+%   SixGird_V2.0
+
+if nargin<2 %输入参数不足时使用默认参数，以免引起异常而使程序停止
+    B=2;
+    S=3;
+elseif nargin<3
+    S=3;
+end
+
+%规则
 K=sum(AdjacentElements);
+
 if K>=B&&K<=S
     State=1;
 else
     State=0;
 end
+
 end
 
